456f71e85bfd137a1d5dd170f05a9d678c26de17
root <root@apcl>
2016-03-21 22:31:24 -0300

    Modificaciones contacto, index, logo

--- theme-hypnos/process.php
+++ theme-hypnos/process.php
@@ -1,19 +1,19 @@
 <?php
- 
-    $to = "wowthemesnet@gmail.com"; 
-    $from = $_REQUEST['email']; 
-    $name = $_REQUEST['name']; 
-    $headers = "From: $from"; 
-    $subject = "You have a message sent from your site: Hypnos"; 
- 
-    $fields = array(); 
-    $fields{"name"} = "name"; 
-    $fields{"email"} = "email"; 
-    $fields{"phone"} = "phone"; 
+
+    $to = "mcastillo@wecontrol.cl";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "Tienes un mensaje enviado desde tu sitio: WeControl";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"phone"} = "phone";
     $fields{"message"} = "message";
- 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
- 
+
+    $body = "Te han enviado lo siguiente:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
     $send = mail($to, $subject, $body, $headers);
- 
+
 ?>
